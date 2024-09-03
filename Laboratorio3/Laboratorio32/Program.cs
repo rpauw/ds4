b1e1776c93fb339e1f1b5ecfea082534d879de2a
rpauw <ricardo.pau@utp.ac.pa>
2024-09-02 22:37:59 -0500

    Add files via upload

---
+++ Laboratorio3/Laboratorio32/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Laboratorio31; // Importa el namespace del proyecto Laboratorio31
+
+namespace Laboratorio32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce el radio del círculo: ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+
+            CalculosMatematicos calculos = new CalculosMatematicos();
+            double area = calculos.CalculoArea(radio);
+
+            Console.WriteLine($"El área del círculo es: {area}");
+        }
+    }
+}
+
---
+++ Laboratorio3/Laboratorio33/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Laboratorio31; // Importa el namespace del proyecto Laboratorio31
+
+namespace Laboratorio33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce el primer lado del rectángulo: ");
+            int lado1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Introduce el segundo lado del rectángulo: ");
+            int lado2 = Convert.ToInt32(Console.ReadLine());
+
+            CalculosMatematicos calculos = new CalculosMatematicos();
+            int perimetro = calculos.CalcularPerimetroRectangulo(lado1, lado2);
+
+            Console.WriteLine($"El perímetro del rectángulo es: {perimetro}");
+        }
+    }
+}
