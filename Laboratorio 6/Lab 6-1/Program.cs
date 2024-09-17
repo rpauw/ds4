0fc9622b5b324964397bfde965134b3967678f08
Ricardo Pau <ricardo.pau@utp.ac.pa>
2024-09-16 20:04:52 -0500

    Laboratorio 6# Completo!!

---
+++ Laboratorio 6/Lab 6-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+        int num;
+
+        Console.WriteLine("Digite el número deseado");
+
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("No ha introducido un dígito válido");
+            num = -1;
+        }
+
+        Console.WriteLine(num);
+    }
+}
---
+++ Laboratorio 6/Lab 6-3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int[] mynumber = { 1, 2, 3 };
+            Console.WriteLine(mynumber[10]); //no existe el indice 10
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Algo salio mal. Valide el indice del arreglo");
+        }
+        finally
+        {
+            Console.WriteLine("Continuación de la aplicacion, luego del bloque try/catch");
+        }
+}
+}
