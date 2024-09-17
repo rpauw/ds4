e81f7ab1b9603f1b87bb0e1cfd6f6c273e30633d
rpauw <ricardo.pau@utp.ac.pa>
2024-09-16 21:56:25 -0500

    Add files via upload

---
+++ Laboratorio 7/Lab 71/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Banco banco1 = new Banco();
+        banco1.Operar();
+        banco1.DepositosTotales();
+        Console.ReadKey();
+    }
+}
---
+++ Laboratorio 7/Lab 72/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        JuegosDeDados juego = new JuegosDeDados();
+        juego.Jugar();
+    }
+}
