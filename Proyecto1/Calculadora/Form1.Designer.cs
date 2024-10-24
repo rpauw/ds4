011272c91e11ec3f09858237432a8d4c272b118f
rpauw <ricardo.pau@utp.ac.pa>
2024-10-24 05:27:12 -0500

    Add files via upload

---
+++ Proyecto1/Calculadora/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace Calculadora
+{
+    partial class btnMostrarCalculos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDisplay = new System.Windows.Forms.TextBox();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btnDecimal = new System.Windows.Forms.Button();
+            this.btnClearEntry = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSquare = new System.Windows.Forms.Button();
+            this.btnSqrt = new System.Windows.Forms.Button();
+            this.dataGridViewCalculos = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCalculos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxDisplay
+            // 
+            this.textBoxDisplay.Location = new System.Drawing.Point(12, 12);
+            this.textBoxDisplay.Multiline = true;
+            this.textBoxDisplay.Name = "textBoxDisplay";
+            this.textBoxDisplay.Size = new System.Drawing.Size(379, 56);
+            this.textBoxDisplay.TabIndex = 0;
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(12, 250);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(71, 38);
+            this.btn0.TabIndex = 1;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(12, 206);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(71, 38);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(89, 206);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(71, 38);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(166, 206);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(71, 38);
+            this.btn3.TabIndex = 4;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(166, 162);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(71, 38);
+            this.btn6.TabIndex = 7;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(89, 162);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(71, 38);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(12, 162);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(71, 38);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(166, 118);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(71, 38);
+            this.btn9.TabIndex = 10;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(89, 118);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(71, 38);
+            this.btn8.TabIndex = 9;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(12, 118);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(71, 38);
+            this.btn7.TabIndex = 8;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btnDecimal
+            // 
+            this.btnDecimal.Location = new System.Drawing.Point(89, 250);
+            this.btnDecimal.Name = "btnDecimal";
+            this.btnDecimal.Size = new System.Drawing.Size(71, 38);
+            this.btnDecimal.TabIndex = 11;
+            this.btnDecimal.Text = ".";
+            this.btnDecimal.UseVisualStyleBackColor = true;
+            // 
+            // btnClearEntry
+            // 
+            this.btnClearEntry.Location = new System.Drawing.Point(89, 74);
+            this.btnClearEntry.Name = "btnClearEntry";
+            this.btnClearEntry.Size = new System.Drawing.Size(71, 38);
+            this.btnClearEntry.TabIndex = 13;
+            this.btnClearEntry.Text = "CE";
+            this.btnClearEntry.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 74);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(71, 38);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(320, 118);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(71, 38);
+            this.btnDivide.TabIndex = 18;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnEquals
+            // 
+            this.btnEquals.Location = new System.Drawing.Point(320, 162);
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.Size = new System.Drawing.Size(71, 127);
+            this.btnEquals.TabIndex = 17;
+            this.btnEquals.Text = "=";
+            this.btnEquals.UseVisualStyleBackColor = true;
+            this.btnEquals.Click += new System.EventHandler(this.btnEquals_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(243, 118);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(71, 38);
+            this.btnMultiply.TabIndex = 16;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(243, 162);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(71, 38);
+            this.btnSubtract.TabIndex = 15;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(243, 206);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(71, 38);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSquare
+            // 
+            this.btnSquare.Location = new System.Drawing.Point(166, 251);
+            this.btnSquare.Name = "btnSquare";
+            this.btnSquare.Size = new System.Drawing.Size(71, 38);
+            this.btnSquare.TabIndex = 19;
+            this.btnSquare.Text = "x²";
+            this.btnSquare.UseVisualStyleBackColor = true;
+            this.btnSquare.Click += new System.EventHandler(this.btnSquare_Click);
+            // 
+            // btnSqrt
+            // 
+            this.btnSqrt.Location = new System.Drawing.Point(243, 251);
+            this.btnSqrt.Name = "btnSqrt";
+            this.btnSqrt.Size = new System.Drawing.Size(71, 38);
+            this.btnSqrt.TabIndex = 20;
+            this.btnSqrt.Text = "√";
+            this.btnSqrt.UseVisualStyleBackColor = true;
+            this.btnSqrt.Click += new System.EventHandler(this.btnSqrt_Click);
+            // 
+            // dataGridViewCalculos
+            // 
+            this.dataGridViewCalculos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCalculos.Location = new System.Drawing.Point(406, 75);
+            this.dataGridViewCalculos.Name = "dataGridViewCalculos";
+            this.dataGridViewCalculos.Size = new System.Drawing.Size(210, 213);
+            this.dataGridViewCalculos.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 38);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Mostrar Cálculos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(463, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 24);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "HISTORIAL";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnMostrarCalculos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 301);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridViewCalculos);
+            this.Controls.Add(this.btnSqrt);
+            this.Controls.Add(this.btnSquare);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnClearEntry);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDecimal);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.textBoxDisplay);
+            this.Name = "btnMostrarCalculos";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCalculos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDisplay;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btnDecimal;
+        private System.Windows.Forms.Button btnClearEntry;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnEquals;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSquare;
+        private System.Windows.Forms.Button btnSqrt;
+        private System.Windows.Forms.DataGridView dataGridViewCalculos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Proyecto1/Calculadora/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Calculadora
+{
+    public partial class btnMostrarCalculos : Form
+    {
+        public btnMostrarCalculos()
+        {
+            InitializeComponent();
+        }
+
+        //Metodo insert y conexión a BD
+        private void GuardarCalculoEnBaseDeDatos(double num1, string operacion, double num2, double resultado)
+        {
+
+           // string connectionString = "Data Source=LAPTOP-VOHABNQ2\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+           // "Data Source=LAPTOP-V0HABNQ2\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True"
+
+
+            using (SqlConnection conn = new SqlConnection("Data Source=LAPTOP-V0HABNQ2\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True"))
+            {
+                conn.Open();
+                string query = "INSERT INTO Calculadora2 (Numero1, Operacion, Numero2, Resultado) VALUES (@num1, @operacion, @num2, @resultado)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@num1", num1);
+                cmd.Parameters.AddWithValue("@operacion", operacion);
+                cmd.Parameters.AddWithValue("@num2", num2);
+                cmd.Parameters.AddWithValue("@resultado", resultado);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        //Mostrar en la pantalla
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            textBoxDisplay.Text += "0";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            textBoxDisplay.Text += "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            textBoxDisplay.Text += "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            textBoxDisplay.Text += "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            textBoxDisplay.Text += "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            textBoxDisplay.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            textBoxDisplay.Text += "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            textBoxDisplay.Text += "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            textBoxDisplay.Text += "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            textBoxDisplay.Text += "9";
+        }
+
+
+        double num1 = 0; //Var qur guarda 1er valor
+        string operation = "";
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBoxDisplay.Text);
+            operation = "+";
+            textBoxDisplay.Clear();
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBoxDisplay.Text);
+            operation = "-";
+            textBoxDisplay.Clear();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBoxDisplay.Text);
+            operation = "*";
+            textBoxDisplay.Clear();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(textBoxDisplay.Text);
+            operation = "/";
+            textBoxDisplay.Clear();
+        }
+
+        private void btnSqrt_Click(object sender, EventArgs e)
+        {
+            double numero = Convert.ToDouble(textBoxDisplay.Text);
+            textBoxDisplay.Text = Math.Sqrt(numero).ToString();
+        }
+
+        private void btnSquare_Click(object sender, EventArgs e)
+        {
+            double numero = Convert.ToDouble(textBoxDisplay.Text);
+            textBoxDisplay.Text = Math.Pow(numero, 2).ToString();
+        }
+
+        //Resultado y parte logica
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            double num2 = Convert.ToDouble(textBoxDisplay.Text); //Variable de 2do valor
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se puede dividir por 0.");
+                        return;
+                    }
+                    break;
+            }
+
+            textBoxDisplay.Text = result.ToString();
+
+            // Para guardar en la base de datos
+            GuardarCalculoEnBaseDeDatos(num1, operation, num2, result);
+        }
+        //Mostrar historial
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection("Data Source=LAPTOP-V0HABNQ2\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True"))
+            {
+                conn.Open();
+                string query = "SELECT Numero1, Operacion, Numero2, Resultado,fecha FROM Calculadora2";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                // dataGridViewCalculos
+                dataGridViewCalculos.DataSource = dt;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBoxDisplay.Clear();
+        }
+    }
+}
